0ac678283b8f0e47e396c17fafe67ffbc6ea2448
Farhaan Zulfikar <240365363@aston.ac.uk>
2025-11-24 10:54:28 +0000

    chore:created all models and controllers for mvp

---
+++ src/app/Http/Controllers/ShipmentsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\shipments;
+use Illuminate\Http\Request;
+
+class ShipmentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(shipments $shipments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(shipments $shipments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, shipments $shipments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(shipments $shipments)
+    {
+        //
+    }
+}
