c6f66d2d928dd5491af3782d155370f5d3bfcc81
DeepakSharma45 <ds774106@gmail.com>
2025-11-24 16:44:14 +0000

    Add contact routes to web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.show');
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.submit');
