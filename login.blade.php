408c0f5026830b7a9cdcbdf609e085260a339a37
Christopher Alozie <christopheralozie3@gmail.com>
2025-11-27 17:21:07 +0000

    Update login.blade.php

--- login.blade.php
+++ login.blade.php
@@ -70,7 +70,7 @@
     <h2>Login</h2>
     <form action="/login" method="POST">
       @csrf
-      <input type="text" placeholder="Username" required />
+      <input type="text" placeholder="Student email" required />
       <input type="password" placeholder="Password" required />
       <button type="submit">Sign In</button>
           @csrf
