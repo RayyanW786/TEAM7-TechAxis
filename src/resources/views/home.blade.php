b4227efbed5c3ea4a697184f5a0f53827727a43a
claymaneuan44 <claymaneuan@gmail.com>
2025-11-29 23:35:55 +0000

    Added hover effect and favicon logo

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -11,6 +11,8 @@ By E
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Tech Axis - Gaming & Tech Products</title>
+<!--ICON FAVI -->
+<link rel="icon" href="/images/TechAxis-LOGO.png" type="image/png">
 <!--FONT-->
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;900&family=Roboto:wght@300;400;700&display=swap" 
         rel="stylesheet">
