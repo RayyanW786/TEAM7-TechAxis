1bb98eb71beae6acb66a816cce735b084d60c26f
Haaris <haaris1112006@yahoo.com>
2025-11-29 00:26:36 +0000

    Account page + layout

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+// For now the account page loads everything (login/register/dashboard)
+
+// Main Page - just shows the account section for now
 Route::get('/', function () {
-    return view('welcome');
+    return view('account');
+});
+
+// Account Page (LOGIN, REGISTER & Dashboard Layout)
+Route::get('/account', function () {
+    return view('account');
 });
