57dbebbd6ff784536ca02666c39a750fb7c601ca
Rayyan <97803050+RayyanW786@users.noreply.github.com>
2025-12-01 11:29:39 +0000

    Revert "Added About Page"

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Home page
 Route::get('/', function () {
     return view('welcome');
 });
-
-// About page
-Route::view('/about', 'about')->name('about');
