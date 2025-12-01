0a57c3e544e9d350701d6806e7f1d65705e6d302
Farhaan Zulfikar <240365363@aston.ac.uk>
2025-12-01 10:18:41 +0000

    Added Admin Dashboard, Created a layout for all the pages including a global css

---
+++ src/resources/views/partials/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="site-footer">
+    <div class="footer-content">
+
+
+        <div class="footer-links">
+            <a href="{{ url('/contact') }}">Contact Us</a>
+            <a href="{{ url('/about') }}">About Us</a>
+
+        </div>
+
+        <p> 2025 Tech Axis. All rights reserved
+        <p>
+        <P> Contact support@techaxis.com || CS2TP Team 7 Project </p>
+    </div>
+</footer>
